45b633acb311403acaac369cd0a2547bf707bf88
Chen <lc890213@gmail.com>
2018-09-27 11:43:35 -0700

    update 76.longest-increasing-subsequence.java

---
+++ 76.longest-increasing-subsequence.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     * @param nums: An integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    public int longestIncreasingSubsequence(int[] nums) {
+        // write your code here
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        return max;
+    }
+}
